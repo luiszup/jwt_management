65f5ee0676065737015fd4f2799ead5e9cdb4d90
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-21 15:16:16 -0300

    chore: first commit

---
+++ src/main/java/br/com/zup/jwt_management/JwtManagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.jwt_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtManagementApplication.class, args);
+	}
+
+}
