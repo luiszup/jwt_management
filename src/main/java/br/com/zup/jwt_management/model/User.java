5b4bbd619016dd28718e0ada1224ffc630eaed4e
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-21 15:37:53 -0300

    feat:add user class model

---
+++ src/main/java/br/com/zup/jwt_management/model/User.java
@@ -0,0 +1,23 @@
+package br.com.zup.jwt_management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
