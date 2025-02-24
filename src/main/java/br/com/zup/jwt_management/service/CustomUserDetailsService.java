b8267c21a83816b8d884ddd36e5ac17c3b64c0a3
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-24 15:09:36 -0300

    feat:add customuserdetailsservice class in service package

---
+++ src/main/java/br/com/zup/jwt_management/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package br.com.zup.jwt_management.service;
+
+import br.com.zup.jwt_management.model.Role;
+import br.com.zup.jwt_management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import br.com.zup.jwt_management.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username) .orElseThrow(() ->
+                new UsernameNotFoundException("User does not exist by username or E-mail"));
+
+        Set<GrantedAuthority> authorities = user.getRoles().stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName()))
+                .collect(Collectors.toSet());
+
+        return new org.springframework.security.core.userdetails.User(
+                username,
+                user.getPassword(),
+                authorities
+        );
+    }
+}
