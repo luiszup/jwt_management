6df5fa85e55b9f84c7705aef5cd21371afd5abd4
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2025-02-26 11:56:35 -0300

    refactor: add endpoints for admin and user

--- src/main/java/br/com/zup/jwt_management/controller/UserController.java
+++ src/main/java/br/com/zup/jwt_management/controller/UserController.java
@@ -2,21 +2,29 @@ package br.com.zup.jwt_management.controller;
 
 import br.com.zup.jwt_management.dtos.RegisterUserDTO;
 import br.com.zup.jwt_management.service.UserService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api")
 public class UserController {
 
-    @Autowired
-    private UserService userService;
+    @GetMapping("/admin")
+    public Map<String, String> admin() {
+        return Map.of("message", "Bem-vindo, administrador!");
+    }
 
-    @PostMapping
-    public void registerUser(@RequestBody RegisterUserDTO registerUserDTO) {
-        userService.registerUser(registerUserDTO);
+    @GetMapping("user")
+    public Map<String, String> user(Authentication authentication) {
+        Claims claims = (Claims) authentication.getDetails();
+        String department = claims.get("department", String.class);
+        return Map.of(
+                "message", "Bem-vindo, " + authentication.getName() + "!",
+                "department", department
+        );
     }
 }
